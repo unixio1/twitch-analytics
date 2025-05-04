26b2913379836a7df57cad44ae11e2c2419a235d
unixio1 <unaxag01@gmail.com>
2025-05-04 12:05:58 +0200

    Main fastapi api structure

---
+++ src/api/__init__.py
@@ -0,0 +1,15 @@
+import importlib
+
+from fastapi import FastAPI
+
+from src.api.analytics import analytics_router
+
+
+def setup_api() -> FastAPI:
+    """Setup the API"""
+
+    api_version = importlib.metadata.version("twitch-analytics")
+    api = FastAPI(title="Twitch analytics API", version=api_version)
+
+    api.include_router(analytics_router, prefix="/analytics/")
+    return api
